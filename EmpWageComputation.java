9c0773f92e934b55a53ae67909814d875757fdc7
RKSunan <rajkumarisunanda@gmail.com>
2020-11-10 12:42:00 +0530

    Compute Daily Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,14 +1,16 @@
 import java.util.Random;
 class EmpWageComputation{
-  public static void main(String args[]) {
+public static void main(String args[]) {
+System.out.println("Day   Wage");
+for (int i=1;i<=20;i++){
 Random rand = new Random();
 int rand_int1 =  rand.nextInt(2);
 if (rand_int1 == 1){
-System.out.println("Employee Present");
+float emp_wage=20*8;
+System.out.println(i + "      " + emp_wage);
 }
 else
-System.out.println("Employee Absent");
+System.out.println(i + "      " + 0);
 }
 }
-
-
+}
