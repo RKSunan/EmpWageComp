1ec2987896bc6b97be291f2aa4dc41e3c6d0a4b4
RKSunan <rajkumarisunanda@gmail.com>
2020-11-10 14:25:18 +0530

    ComputeDailyEmpWagewithPartTime

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -6,9 +6,15 @@ for (int i=1;i<=20;i++){
 Random rand = new Random();
 int rand_int1 =  rand.nextInt(2);
 if (rand_int1 == 1){
+ int rand_int2 =  rand.nextInt(2);
+if (rand_int2 == 1){
 float emp_wage=20*8;
-System.out.println(i + "      " + emp_wage);
+System.out.println(  i  + "   "   + emp_wage);
 }
+else{
+float emp_wage = 20*4;
+System.out.println(  i  + "   "   + emp_wage);
+}}
 else
 System.out.println(i + "      " + 0);
 }
